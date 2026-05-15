b570fe855644d539bc703cad26b93d627230696e
JakubAdam28 <jakub.adam.023@pslib.cz>
2026-05-15 14:10:59 +0200

    optimalizace provozu

--- src/config/webrtc.ts
+++ src/config/webrtc.ts
@@ -1,20 +1,6 @@
 export const ICE_SERVERS: RTCIceServer[] = [
   { urls: "stun:stun.l.google.com:19302" },
-  { urls: "stun:stun1.l.google.com:19302" },
-  { urls: "stun:stun2.l.google.com:19302" },
-  { urls: "stun:stun3.l.google.com:19302" },
-  { urls: "stun:stun4.l.google.com:19302" },
   { urls: "stun:stun.relay.metered.ca:80" },
-  {
-    urls: "turn:global.relay.metered.ca:80",
-    username: "97b6737bc142e0519a6d6bda",
-    credential: "aqwbmHxp5fobShAc",
-  },
-  {
-    urls: "turn:global.relay.metered.ca:80?transport=tcp",
-    username: "97b6737bc142e0519a6d6bda",
-    credential: "aqwbmHxp5fobShAc",
-  },
   {
     urls: "turn:global.relay.metered.ca:443",
     username: "97b6737bc142e0519a6d6bda",
@@ -29,5 +15,5 @@ export const ICE_SERVERS: RTCIceServer[] = [
 
 export const RTC_CONFIG: RTCConfiguration = {
   iceServers: ICE_SERVERS,
-  iceCandidatePoolSize: 10,
+  iceCandidatePoolSize: 0,
 };
